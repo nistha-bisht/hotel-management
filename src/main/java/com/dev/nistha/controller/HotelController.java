428aa58b6eac63c2efe4f1033c8c72fcb979cb6e
Nistha Bisht <nisthabishtnb@gmail.com>
2024-10-28 20:37:04 +0530

    Add changes for list hotels API
     - Add dummy implementation of List All Hotels API.
     - Refactor typo errors.

--- src/main/java/com/dev/nistha/controller/HotelController.java
+++ src/main/java/com/dev/nistha/controller/HotelController.java
@@ -4,13 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/hotel")
 public class HotelController {
 
     @GetMapping("/detail")
     public String getHotel() {
-        return "Raddison Blu";
+        return "Radisson Blu";
+    }
+
+    @GetMapping
+    public List<Map<String, String>> listHotels() {
+        Map<String, String> hotels = new LinkedHashMap<>();
+
+        hotels.put("name", "Radisson Blu");
+
+        return List.of(hotels);
     }
 
 }
